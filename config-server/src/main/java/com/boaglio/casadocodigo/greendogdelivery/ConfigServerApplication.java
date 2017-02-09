5fd3f10ec1811d2ef6f67d7402e27f5478674798
Fernando Boaglio <boaglio@gmail.com>
2017-02-09 01:33:12 +0000

    cloud ok

---
+++ config-server/src/main/java/com/boaglio/casadocodigo/greendogdelivery/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.boaglio.casadocodigo.greendogdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringCloudApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
---
+++ eureka-server/src/main/java/com/boaglio/casadocodigo/greendogdelivery/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.boaglio.casadocodigo.greendogdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringCloudApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
